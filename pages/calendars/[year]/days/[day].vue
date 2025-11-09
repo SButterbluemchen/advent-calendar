f4383a3ad37fd492b6c4926e21d1f597f27079c0
Damien Walle <damien.walle@gmail.com>
2025-11-09 15:10:14 +0100

    feat(image): add nas url and image module

--- pages/calendars/[year]/days/[day].vue
+++ pages/calendars/[year]/days/[day].vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+const { nasUrl } = useRuntimeConfig()
 
+const myUrl = computed(() => {
+  return `${nasUrl}/louise/2025/bg.png`
+})
 </script>
 
 <template>
   <p>Photo</p>
+  <NuxtImg :src="myUrl" />
 </template>
 
 <style scoped>
