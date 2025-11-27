1e1748850a46623dc1b77f7b88782171ac75ad56
Damien Walle <damien.walle@gmail.com>
2025-11-27 21:41:41 +0100

    fix(background): try to fix background

--- layouts/day-layout.vue
+++ layouts/day-layout.vue
@@ -7,8 +7,12 @@ const route = useRoute()
 const currentYear = new Date().getFullYear()
 const yearFromUrl = computed(() => Number(route.params.year))
 
+const img = useImage()
+
 const backgroundImage = computed(() => {
-  return `url(${nasUrl}/${user}/${yearFromUrl.value}/background.jpg)`
+  const imgUrl = img(`${nasUrl}/${user}/${yearFromUrl.value}/background.jpg`)
+
+  return `url(${imgUrl})`
 })
 
 const style = computed(() => ({
