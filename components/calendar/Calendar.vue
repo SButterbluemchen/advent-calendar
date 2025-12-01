ec675a3cc298a35b3284625c5589862de41aff9c
Damien Walle <damien.walle@gmail.com>
2025-12-01 15:11:15 +0100

    fix(style): problem cover

--- components/calendar/Calendar.vue
+++ components/calendar/Calendar.vue
@@ -8,7 +8,7 @@ const days = ref(shuffledArray)
 </script>
 
 <template>
-  <article class="grid p-1 grid-cols-3 gap-x-[1.25rem] gap-y-[0.75rem] sm:grid-cols-4 sm:gap-x-[3rem] sm:gap-y-[3rem] md:grid-cols-6 md:gap-x-[2rem] md:gap-y-[2rem] lg:grid-cols-6 lg:gap-y-[2rem] xl:gap-y-[3rem] xl:gap-x-[6rem]">
+  <article class="grid p-1 grid-cols-4 gap-x-[1.25rem] gap-y-[0.75rem] sm:grid-cols-4 sm:gap-x-[3rem] sm:gap-y-[3rem] md:grid-cols-6 md:gap-x-[2rem] md:gap-y-[2rem] lg:grid-cols-6 lg:gap-y-[2rem] xl:gap-y-[3rem] xl:gap-x-[6rem]">
     <ClientOnly>
       <Card
         v-for="day in days" :key="day" :day="day"
